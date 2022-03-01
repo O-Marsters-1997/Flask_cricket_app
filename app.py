05eed34d8aeb59c796b23578e6af73e99e47edbe
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2022-03-01 17:52:29 +0000

    added sqla alchemy

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 # from livereload import Server
 from controllers.table_controller import table_blueprint
 from controllers.teams_controller import teams_blueprint
@@ -17,6 +18,7 @@ else:
     app.debug= False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://cpznoflvpdznkq:ff678a0d848c30b6d4fa86484024ad9fc63a56b5d49a2fe2eb42df386a70c265@ec2-18-204-131-56.compute-1.amazonaws.com:5432/d4ion1dim6m1jc'
 
+db = SQLAlchemy(app)
 
 app.register_blueprint(table_blueprint)
 app.register_blueprint(teams_blueprint)
