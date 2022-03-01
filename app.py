f03a9db671dbc79cf6f8b3999e250f1193cff5b1
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2022-03-01 16:55:29 +0000

    added dependnecnies

--- app.py
+++ app.py
@@ -8,6 +8,16 @@ from controllers.games_controller import games_blueprint
 # from controllers.book_controller import books_blueprint
 app = Flask(__name__)
 
+ENV = 'prod'
+
+if ENV =='dev':
+    app.debug= True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'http://localhost:5000'
+else:
+    app.debug= False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ftyceuqgtqwfeu:27da0604226e180b6266c1364c2be036531ddecf08a594d656e74c9b40fe6957@ec2-99-80-108-106.eu-west-1.compute.amazonaws.com:5432/da2p5f62l76kqk'
+
+
 app.register_blueprint(table_blueprint)
 app.register_blueprint(teams_blueprint)
 app.register_blueprint(games_blueprint)
